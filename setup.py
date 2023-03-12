e16eeda7d33c3a72e7f414703ca8b22c93ea098f
inti <inti.albuquerque@trademe.co.nz>
2023-03-13 11:46:17 +1300

    asses setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='scrapper',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='ialbuquerque',
+    author_email='',
+    description=''
+)
