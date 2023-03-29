46d3a36a1c4ee2adb5dba3b09ffb0acd09c91733
inti <inti.albuquerque@trademe.co.nz>
2023-03-29 22:56:14 +1300

    creating dictionariy to upload to database

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ class SupportedStores(Enum):
     PackNSave = "packNSave"
     countdown = "Countdown"
 
+class ConcatcKeys(Enum):
+    newWorldItems = "newWorldItems"
+    PackNSaveItems = "PackNSaveItems"
+    countdownItems = "countdownItems"
+    newWorldPrice = "newWorldPrice"
+    PackNSavePrice = "PackNSavePrice"
+    countdownPrice = "countdownPrice"
+    category = "category"
+    brand = "brand"
 
 def createTables():
     db = Database()
@@ -52,18 +61,36 @@ def clusterData():
         if ckm in nwK:
             for coutDownItem in countdownDict[ck]:
                 name1 = finalCategories.transformItem(coutDownItem["name"], stopSet)
-                items[name1] = {"name": [coutDownItem["name"]], "nwp": "0", "cdp": coutDownItem["price"], "category": coutDownItem["category"]}
+                if name1 in items:
+                    items[name1][ConcatcKeys.countdownItems.value].append(coutDownItem["name"])
+                    items[name1][ConcatcKeys.countdownPrice.value].append(coutDownItem["price"])
+                else:
+                    items[name1] = {}
+                    items[name1][ConcatcKeys.countdownItems.value] = [coutDownItem["name"]]
+                    items[name1][ConcatcKeys.countdownPrice.value] = [coutDownItem["price"]]
+                    items[name1][ConcatcKeys.newWorldPrice.value] = []
+                    items[name1][ConcatcKeys.newWorldItems.value] = []
+                    items[name1][ConcatcKeys.category.value] = coutDownItem["category"]
+
                 for newWorldItem in newWorldDict[nwK[ckm]]:
                     name2 = finalCategories.transformItem(newWorldItem["name"], stopSet)
                     if name1 in name2 or name2 in name1 or name2 == name1:
-                        items[name1]["nwp"] = newWorldItem["price"]
-                        items[name1]["name"].append(newWorldItem["name"])
+                        items[name1][ConcatcKeys.newWorldPrice.value].append(newWorldItem["price"])
+                        items[name1][ConcatcKeys.newWorldItems.value].append(newWorldItem["name"])
                     else:
-                        items[name2] = {"name": [newWorldItem["name"]], "nwp": newWorldItem["price"], "cdp": "0",
-                                        "category": newWorldItem["category"]}
+                        items[name2] = {}
+                        items[name2][ConcatcKeys.newWorldItems.value] = [newWorldItem["name"]]
+                        items[name2][ConcatcKeys.newWorldPrice.value] = [newWorldItem["price"]]
+                        items[name2][ConcatcKeys.countdownItems.value] = []
+                        items[name2][ConcatcKeys.countdownPrice.value] = []
+                        items[name2][ConcatcKeys.category.value] = newWorldItem["category"]
+
     for item in items.values():
-        print(item["name"])
-        print(item["category"])
+        if item[ConcatcKeys.countdownItems.value]:
+            print(item[ConcatcKeys.countdownItems.value])
+        if item[ConcatcKeys.newWorldItems.value]:
+            print(item[ConcatcKeys.newWorldItems.value])
+        print(item[ConcatcKeys.category.value])
         print("-"*100)
 
 
